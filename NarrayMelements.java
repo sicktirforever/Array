e7bff22ea97a8a59b08e5bff7a92b4ca8ac2668e
sicktirforever <45896295+sicktirforever@users.noreply.github.com>
2019-01-11 20:48:44 +0200

    Add files via upload

--- NarrayMelements.java
+++ NarrayMelements.java
@@ -12,7 +12,7 @@ public class NarrayMelements {
         System.out.print("Introduceti numarul de array-uri: ");
         int N = sc.nextInt();
         int[] array = new int[N];
-
+        int[] sumaArray = new int[N];
         for (int i = 0; i < N; i++) {
 
             System.out.print("Introduceti numarul de elemente din array: ");
@@ -23,24 +23,32 @@ public class NarrayMelements {
                 System.out.print("tempArray [ " + i + " ] [ " + j + "] = ");
                 tempArray[j] = sc.nextInt();
                 array[i] = tempArray[j];
-            }
-            int[] sumaArray = new int[N];
-            for (int numbers : tempArray) {
-                suma += numbers;
+
             }
 
-            if (sumaArray[i] == 0) {
-                sumaArray[i] = suma;
-
-                System.out.println(Arrays.toString(sumaArray));
+            suma = 0;
+            for (int j = 0; j < M; j++) {
+                suma = suma + tempArray[j];
             }
-            System.out.println(suma);
 
+
+            sumaArray[i] = suma;
+
+            System.out.println(Arrays.toString(sumaArray));
+        }
+        for (int i = 0; i < sumaArray.length; i++) {
+            if (sumaArray[i] != sumaArray[sumaArray.length - i - 1]) {
+                System.out.println("Sumele nu sunt egale");
+            } else {
+                System.out.println("Sumele sunt egale");
+            }
+            break;
         }
 
+
+
+
     }
-
-
 }
 
 
